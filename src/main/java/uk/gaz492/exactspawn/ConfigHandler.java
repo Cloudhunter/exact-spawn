6e92fd8e2de304c77923dc2aa10fad6b842f8d2a
Gareth Williams <gaz492@gmail.com>
2018-10-22 02:06:01 +0100

    Cleanup and better comments in config

--- src/main/java/uk/gaz492/exactspawn/ConfigHandler.java
+++ src/main/java/uk/gaz492/exactspawn/ConfigHandler.java
@@ -1,18 +1,21 @@
 package uk.gaz492.exactspawn;
 
 import net.minecraftforge.common.config.Config;
-import net.minecraftforge.fml.common.Mod;
 
 @Config(modid = ExactSpawn.MODID)
 public class ConfigHandler {
     public static SubCategory spawnSettings = new SubCategory();
 
     public static class SubCategory {
-        @Config.Comment({
-                "Allow you to set the pitch and yaw of the player when they spawn",
-                "Only works for when they spawn at the global spawn"
-        })
+
+        @Config.RequiresWorldRestart
+        @Config.Comment({"Sets the player camera angle to look up or down"})
+        @Config.RangeDouble(min = -90, max = 90)
         public float spawnRotationPitch;
+
+        @Config.RequiresWorldRestart
+        @Config.Comment({"Sets the player rotation"})
+        @Config.RangeDouble(min = 0, max = 359)
         public float spawnRotationYaw;
     }
 }
--- src/main/java/uk/gaz492/exactspawn/ExactSpawn.java
+++ src/main/java/uk/gaz492/exactspawn/ExactSpawn.java
@@ -2,11 +2,9 @@ package uk.gaz492.exactspawn;
 
 import net.minecraft.world.DimensionType;
 import net.minecraftforge.common.DimensionManager;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
