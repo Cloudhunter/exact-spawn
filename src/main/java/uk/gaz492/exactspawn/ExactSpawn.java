1293c367332b026e35901549aaac6940862438a3
Gareth Williams <gaz492@gmail.com>
2019-01-06 19:48:28 +0000

    Code cleanup

--- src/main/java/uk/gaz492/exactspawn/ExactSpawn.java
+++ src/main/java/uk/gaz492/exactspawn/ExactSpawn.java
@@ -5,22 +5,13 @@ import net.minecraftforge.common.DimensionManager;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
-import org.apache.logging.log4j.Logger;
 import uk.gaz492.exactspawn.command.CommandExactSpawn;
 
 @Mod(modid = ExactSpawn.MODID, name = ExactSpawn.NAME, version = "@MOD_VERSION@", dependencies = "after:yunomakegoodmap", acceptableRemoteVersions = "*")
 public class ExactSpawn {
     public static final String MODID = "exactspawn";
     public static final String NAME = "Exact Spawn";
-
-    private static Logger logger;
-
-    @EventHandler
-    public void preInit(FMLPreInitializationEvent event) {
-        logger = event.getModLog();
-    }
 
     @EventHandler
     public void load(FMLInitializationEvent event) {
